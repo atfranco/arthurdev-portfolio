07da5392b92739757f12716cbcff28bc96d46ae3
tutao <artfco@yahoo.com.br>
2023-04-15 22:19:31 -0300

    debug

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,9 +1,23 @@
-import Graficoforce from '../components/Chartforce'
+import AnimatedText from 'react-animated-text-content';
 
 export default function Skills() {
     return(
         <div>
-            <Graficoforce />
+            <AnimatedText
+            type="chars" // animate words or chars
+            animation={{
+                ease: 'ease-in-out',
+            }}
+            animationType="throw"
+            interval={0.1}
+            duration={1.5}
+            tag="p"
+            className="animado-titulo"
+            includeWhiteSpaces
+            threshold={0.1}
+            rootMargin="50%">
+            Skills
+            </AnimatedText>
         </div>
 
     )
