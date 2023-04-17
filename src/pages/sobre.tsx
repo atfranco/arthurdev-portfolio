4c02c6a3a46a7021cb6b49bf5337fc301eb5e28b
tutao <artfco@yahoo.com.br>
2023-04-17 13:12:55 -0300

    espacer

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -54,6 +54,7 @@ export default function Sobre() {
             rootMargin="50%">
             Breve Resumo
             </AnimatedText>
+            <Espacador />
             <p className='texto'>I am interested in freelance opportunities - especially on ambitious or large projects. However, if you have any other requests or questions, don't hesitate to contact me using below form either.</p>
             <Espacador />
             <div className="wrappercarrousel">
