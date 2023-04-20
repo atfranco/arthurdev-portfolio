5ab2ba4e697d14e2870decd64938606c6f0b7665
tutao <artfco@yahoo.com.br>
2023-04-20 06:08:04 -0300

    works teste

---
+++ src/pages/works.tsx
@@ -0,0 +1,45 @@
+import styles from '@/styles/Posts.module.css';
+import Cardportfolio from '@/components/Cardportfolio';
+import Espacador from '@/components/Espacador';
+
+
+type Data = {
+    name: string;
+};
+
+type Works = {
+    id: string;
+    nome: string;
+    Works: any;
+    descricao: any;
+};
+
+export async function getStaticProps() {
+    const data = await fetch('http://localhost:3000/api/trabalhos')
+    const works = await data.json()
+    console.log(works)
+    return {
+        props: { works }
+    }
+}
+
+export default function Works({ works }) {
+    return (
+        <>
+        <div className='tela'>
+            <div className='esquerda'>
+                <h1>Trabalhos recentes</h1>
+                <Espacador />
+            </div>
+            <div className='direita'>
+                <div className={styles.formata}>
+                    <div className={styles.coluna}>
+                    {works.map((work) => 
+                    (<Cardportfolio key={works.id} work={work} />))}
+                    </div>
+                </div>                
+            </div>
+        </div>
+        </>
+    )
+}
