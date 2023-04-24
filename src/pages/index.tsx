ffcaf51f3327c6a9ccc855b3b9f8d6b73408ef66
tutao <artfco@yahoo.com.br>
2023-04-23 22:57:45 -0300

    update meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,22 @@ import Espacador from '@/components/Espacador';
 export default function Home() {
   return (
     <>
+      <Head>
+      <title>Arthurdev.tech | Desenvolvedor front end + Designer Gráfico</title>
+      <meta name="description" content="Pronto para atender empresas e profissionais liberais que busquem inclusão digital, automação e tecnologia de ponta. Com vasta experiência na criação Sites, E-commerces, Manipulação de dados e Integração de Sistemas." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta property="og:title" content="Arthurdev.tech | Desenvolvedor front end + Designer Gráfico"/>
+      <meta property="og:image" content="https://arthurdev-portfolio.vercel.app/screenthumb.jpg"/>
+      <meta property="og:url" content="https://arthurdev.tech"/>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta property="og:description" content="Pronto para atender empresas e profissionais liberais que busquem inclusão digital, automação e tecnologia de ponta. Com vasta experiência na criação Sites, E-commerces, Manipulação de dados e Integração de Sistemas."/>
+      <meta property="og:site_name" content="Arthurdev.tech | Desenvolvedor front end + Designer Gráfico"/>
+      <meta name="twitter:image:alt" content="Conheça o portfólio"/>
+      <meta property="fb:app_id" content="your_app_id"/>
+      <meta name="twitter:site" content="@tutaoo"/>
+      <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+      </Head>
       <div className='tela'>
         <div className='esquerda'>
           <AnimatedText
