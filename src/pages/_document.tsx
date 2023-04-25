9afe53a530abf960c1d523e410d61ab7362dc6f1
tutao <artfco@yahoo.com.br>
2023-04-25 15:34:22 -0300

    gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+declare var dataLayer: any[];
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,7 +9,15 @@ export default function Document() {
       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
      crossOrigin=""/>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.min.js"></script>     
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.min.js"></script>  
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-BPWBZZY9R9"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BPWBZZY9R9');
+      </script>   
      </Head>
       <body>
         <Main />
