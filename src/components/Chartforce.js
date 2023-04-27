4dee4f94042c2bfffea15f7306dd261a99c578c3
tutao <artfco@yahoo.com.br>
2023-04-27 13:31:03 -0300

    skillsrating

--- src/components/Chartforce.js
+++ src/components/Chartforce.js
@@ -16,71 +16,71 @@ class Graficoforce extends Component {
         series.data = [
             {
               name: "HABILIDADES",
-              value: 150,
+              value: 100,
               children: [
                 {
                   name: "FRONTEND",
-                  value: 104,
+                  value: 80,
                   children: [
                     {
-                      name: "CSS", value: 80, children: [
-                        { name: "SCSS", value: 60 },
-                        { name: "TAILWIND", value: 60 },
-                        { name: "BOOTSTRAP", value: 60 },
+                      name: "CSS", value: 50, children: [
+                        { name: "Saas", value: 25 },
+                        { name: "Tailwind", value: 25 },
+                        { name: "Bootstrap", value: 25 },
                       ]
                     },
                     {
-                      name: "JS", value:80, children: [
-                        { name: "REACT", value: 70, children: [
-                          { name: "STYLED COMP", value: 50 },
-                          { name: "MATERIAL UI", value: 50 },
-                          { name: "HOOKS", value: 50 },
-                          { name: "NEXT.JS", value: 50 }
+                      name: "JS", value:50, children: [
+                        { name: "REACT", value: 50, children: [
+                          { name: "Styled Components", value: 25 },
+                          { name: "Material UI", value: 25 },
+                          { name: "Hooks", value: 25 },
+                          { name: "Next.js", value: 25 }
                           ]
                     },
-                        { name: "TYPESCRIPT", value: 50 },
-                        { name: "THREE.JS", value: 50 }
+                        { name: "TYPESCRIPT", value: 35 },
+                        { name: "THREE.JS", value: 25 }
                       ]
                     },
-                    { name: "HTML5", value: 70 },
+                    { name: "HTML5", value: 50 },
                   ]
                 },
                 {
                   name: "MKT",
-                  value: 103,
+                  value: 40,
                   children: [  
-                      { name: "SEO", value: 40},
-                      { name: "ANALYTICS", value: 30},
-                      { name: "INSTAGRAM ADS", value: 40},
+                      { name: "SEO", value: 25},
+                      { name: "Google Analytics", value: 25},
+                      { name: "Instagram ADS", value: 25},
                   ]
                 },
                 {
                   name: "BACKEND",
-                  value: 102,
+                  value: 70,
                   children: [
                     {
-                      name: "API'S", value: 80, children: [
-                        { name: "REST/API", value: 60 },
-                        { name: "FUNÇÕES", value: 50 },
-                        { name: "INTEGRAR DADOS", value: 40 }
+                      name: "API'S", value: 40, children: [
+                        { name: "REST/API", value: 25 },
+                        { name: "Microserviços", value: 25 },
+                        { name: "Integração de Dados", value: 25 }
                       ]
                     },
                     {
-                      name: "BANCO DE DADOS", value: 70, children: [
-                        { name: "MYSQL", value: 50 },
-                        { name: "NOSQL", value: 50},
+                      name: "BANCO DE DADOS", value: 40, children: [
+                        { name: "MySQL", value: 25 },
+                        { name: "NoSQL", value: 25},
                       ]
                     },
                       {
-                      name: "LINGUAGENS", value: 70,
+                      name: "LINGUAGENS", value: 40,
                        children: [
-                        { name: "JS", value: 70, children: [
-                            {name: "NODE", value: 40},
-                            {name: "Jest", value: 30},
-                            {name: "Nest/Express", value: 40}
+                        { name: "JS", value: 40, children: [
+                            {name: "Node.js", value: 25},
+                            {name: "Jest", value: 25},
+                            {name: "Nest/Express", value: 25}
                         ]},
-                        { name: "PHP", value: 70, children: [
-                            {name: "WORDPRESS"}
+                        { name: "PHP", value: 40, children: [
+                            {name: "Wordpress", value: 35}
                         ] },
                       ]
                     }
@@ -88,11 +88,11 @@ class Graficoforce extends Component {
                 },
                 {
                     name: "DESIGN",
-                    value: 100,
+                    value: 60,
                     children: [  
-                        { name: "CRIAÇÃO DE LOGOTIPOS", value: 60},
-                        { name: "IDENTIDADE VISUAL", value: 70},
-                        { name: "UI/UX", value: 80},
+                        { name: "Criação de Logotipos", value: 40},
+                        { name: "UI/UX", value: 40},
+                        { name: "Prototipagem / Figma", value: 40},
                     ]
                   },         
               ]
@@ -106,9 +106,9 @@ class Graficoforce extends Component {
         //series.nodes.template.events.on("hit", function(event) {chart.zoomToDataItem(event.target.dataItem, 1.2, true)});
         series.maxLevels = 5;
         series.fontSize = 16;
-        series.minRadius = 30;
+        series.minRadius = 0;
         series.maxRadius = 100;
-        series.links.template.distance = 1.1;
+        series.links.template.distance = 1.3;
         series.links.template.tooltipText = "{name}: [bold]{value}[/]";
         series.links.template.interactionsEnabled = true;
         series.links.template.strokeWidth = 5;
